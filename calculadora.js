28a64ecd0f359b6d3d6202c2b888c2b34d266d9d
JEAN PAUL DA SILVA <06448551000@senacrs.edu.br>
2026-03-16 15:05:45 -0300

    subtração

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,5 @@
-let n1 = 10
-let n2 = 5
+function calcular(n1, n2){
+  return n1 - n2;
+}
 
-console.log("somar", n1 + n2)
-console.log("subtração", n1 - n2)
-console.log("multiplicação", n1 * n2)
-console.log("divisão", n1 / n2)
+console.log("Subtração:", calcular(60, 35));
