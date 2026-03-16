c472f7ab6a7f41b24d2bf0d4e3a986dd984aef33
JEAN PAUL DA SILVA <06448551000@senacrs.edu.br>
2026-03-16 15:21:12 -0300

    multiplicar

--- calculadora.js
+++ calculadora.js
@@ -9,3 +9,10 @@ function subtrair(n1,n2){
     return n1 - n2;
 }
 console.log(subtrair(65,80))
+
+//---------------------------------------
+
+function multiplicar(n1,n2){
+    return n1 * n2;
+}
+console.log(multiplicar(65,80))
